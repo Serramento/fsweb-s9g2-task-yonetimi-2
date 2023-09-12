41c6863edd0ff5af5400942e5232a58065a9c07a
Serramento <134259414+Serramento@users.noreply.github.com>
2023-09-12 11:41:29 +0300

    S9G2 tamamlandı

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        maUrgent : "#ffd9d4",
+        maNormal : "#d4d7ff"
+      }
+    },
+  },
+  plugins: [],
+}
